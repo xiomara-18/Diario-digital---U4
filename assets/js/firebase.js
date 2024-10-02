88c43c2f2990bd30fdc81e4b6dd4d6d6fdad6c89
xio18meza <xiomarastefani15@gmail.com>
2024-10-02 17:49:16 -0600

    first commit

---
+++ assets/js/firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAtusA2GL8S3zipfYwjNuDT7kvAqQSKwgo",
+    authDomain: "diario-digital-210e8.firebaseapp.com",
+    projectId: "diario-digital-210e8",
+    storageBucket: "diario-digital-210e8.appspot.com",
+    messagingSenderId: "25577141208",
+    appId: "1:25577141208:web:27b0f509c419302c86b8ad"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//Importamos la app
+import {app} from "./firebase.js";
+
+console.log(app);

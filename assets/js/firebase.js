5c09bb7515ae72f4992f467f3737f6e9c21448ea
xio18meza <xiomarastefani15@gmail.com>
2024-10-02 21:00:55 -0600

    Actualización de documento

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,6 +1,10 @@
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+// Autenticación
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
+
+
+   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
@@ -14,4 +18,7 @@
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+   export const app = initializeApp(firebaseConfig);
+
+   // Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
